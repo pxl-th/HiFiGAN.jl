d214846fe0c6ec01ac247632e819fb6345e8bf8e
Anton Smirnov <tonysmn97@gmail.com>
2024-06-11 18:36:23 +0300

    Finish training

--- src/HiFiGAN.jl
+++ src/HiFiGAN.jl
@@ -1,6 +1,7 @@
 module HiFiGAN
 
-using AMDGPU
+# using AMDGPU
+using CUDA
 using KernelAbstractions
 using NNlib
 using Flux
@@ -10,6 +11,7 @@ using Random
 using Statistics
 using ProgressMeter
 
+import JLD2
 import MLUtils
 
 include("spectral.jl")
@@ -19,12 +21,20 @@ include("discriminator.jl")
 include("loss.jl")
 
 function main()
-    train_files, test_files = load_files("/home/pxlth/Downloads/LJSpeech-1.1/metadata.csv")
+    output_dir = "/home/pxl-th/code/HiFiGAN.jl/runs"
+    states_dir = joinpath(output_dir, "states")
+    val_dir = joinpath(output_dir, "validations")
+    isdir(output_dir) || mkpath(output_dir)
+    isdir(states_dir) || mkpath(states_dir)
+    isdir(val_dir) || mkpath(states_dir)
+
+    # train_files, test_files = load_files("/home/pxlth/Downloads/LJSpeech-1.1/metadata.csv")
+    train_files, test_files = load_files("/home/pxl-th/code/datasets/LJSpeech-1.1/metadata.csv")
     train_dataset = LJDataset(train_files)
     test_dataset = LJDataset(test_files)
 
-    train_loader = MLUtils.DataLoader(train_dataset; batchsize=4)
-    test_loader = MLUtils.DataLoader(test_dataset; batchsize=4)
+    train_loader = MLUtils.DataLoader(train_dataset; batchsize=1, shuffle=true)
+    test_loader = MLUtils.DataLoader(test_dataset; batchsize=1)
     @info "Train loader length: $(length(train_loader))"
     @info "Test loader length: $(length(test_loader))"
 
@@ -35,26 +45,49 @@ function main()
 
         resblock_kernels=[3, 5, 7],
         resblock_dilations=[[1, 2], [2, 6], [3, 12]],
-    ) |> gpu
-    period_discriminator = MultiPeriodDiscriminator() |> gpu
-    scale_discriminator = MultiScaleDiscriminator() |> gpu
+    )
+    period_discriminator = MultiPeriodDiscriminator()
+    scale_discriminator = MultiScaleDiscriminator()
 
     opt_generator = Flux.setup(AdamW(2e-4), generator)
     opt_period_discriminator = Flux.setup(AdamW(2e-4), period_discriminator)
     opt_scale_discriminator = Flux.setup(AdamW(2e-4), scale_discriminator)
 
-    # TODO: load from checkpoint
+    states = readdir(states_dir)
+    if !isempty(states)
+        states = sort(states; by=i -> parse(Int, split(i, "-")[2]))
+        ckpt_path = joinpath(states_dir, states[end])
+        @info "Loading checkpoint: `$ckpt_path`."
+        ckpt = JLD2.load(ckpt_path)
+
+        opt_generator = ckpt["opt_generator"]
+        opt_period_discriminator = ckpt["opt_period_discriminator"]
+        opt_scale_discriminator = ckpt["opt_scale_discriminator"]
+
+        Flux.loadmodel!(generator, ckpt["generator"])
+        Flux.loadmodel!(period_discriminator, ckpt["period_discriminator"])
+        Flux.loadmodel!(scale_discriminator, ckpt["scale_discriminator"])
+    end
+
+    generator = generator |> gpu
+    period_discriminator = period_discriminator |> gpu
+    scale_discriminator = scale_discriminator |> gpu
+
+    opt_generator = opt_generator |> gpu
+    opt_period_discriminator = opt_period_discriminator |> gpu
+    opt_scale_discriminator = opt_scale_discriminator |> gpu
 
     steps = 0
+    last_epoch = 0
     epochs = 3000
     save_step = 1000
     test_step = 100
 
     mel_transform = gpu(train_dataset.mel_transform_loss)
+    gloss, dloss, vloss = 0f0, 0f0, 0f0
 
     @info "Training for $epochs epochs..."
-
-    for epoch in 1:epochs # TODO load last epoch
+    for epoch in (max(1, last_epoch)):epochs
         bar = Progress(length(train_loader); desc="[$epoch / $epochs] Training")
         for batch in train_loader
             gloss, dloss = train_step(batch,
@@ -62,21 +95,32 @@ function main()
                 opt_generator, opt_period_discriminator, opt_scale_discriminator,
                 mel_transform)
 
-            showvalues = [(:GLoss, gloss), (:DLoss, dloss)]
-
             if steps % test_step == 0
-                val_loss = validation_step(generator, test_loader; mel_transform)
-                push!(showvalues, (:VLoss, val_loss))
+                vloss = validation_step(generator, test_loader;
+                    mel_transform, val_dir, current_step=steps)
             end
 
-            next!(bar; showvalues)
+            if steps % save_step == 0
+                JLD2.jldsave(joinpath(states_dir, "ckpt-$epoch-$steps.jld2");
+                    generator=Flux.state(generator |> cpu),
+                    period_discriminator=Flux.state(period_discriminator |> cpu),
+                    scale_discriminator=Flux.state(scale_discriminator |> cpu),
 
-            # TODO save
+                    opt_generator=cpu(opt_generator),
+                    opt_period_discriminator=cpu(opt_period_discriminator),
+                    opt_scale_discriminator=cpu(opt_scale_discriminator),
+                    last_epoch,
+                )
+            end
 
+            next!(bar; showvalues=[(:GLoss, gloss), (:DLoss, dloss), (:VLoss, vloss)])
             steps += 1
         end
+        last_epoch = epoch
     end
 
+    # TODO plot loss over time and save
+    # TODO visualize mel spectrograms?
     return
 end
 
@@ -133,9 +177,12 @@ function train_step(
     return gloss, dloss
 end
 
-function validation_step(generator, test_loader; mel_transform)
+function validation_step(
+    generator, test_loader;
+    mel_transform, val_dir, current_step::Int,
+)
     total_loss = 0f0
-    @showprogress desc="Validating" for batch in test_loader
+    @showprogress desc="Validating" for (i, batch) in enumerate(test_loader)
         wavs, mel, mel_loss = gpu.(batch)
 
         ŷ = generator(mel)
@@ -143,7 +190,10 @@ function validation_step(generator, test_loader; mel_transform)
         ŷ_mel = mel_transform(reshape(ŷ, (size(ŷ)[[1, 3]])))
         total_loss += Flux.mae(ŷ_mel, mel_loss)
 
-        # TODO save first few samples to disk
+        if i ≤ 4
+            save(joinpath(val_dir, "real-$current_step-$i.flac"), reshape(cpu(wavs), size(wavs, 1), 1), 16000)
+            save(joinpath(val_dir, "gen-$current_step-$i.flac"), reshape(cpu(ŷ), size(ŷ, 1), 1), 16000)
+        end
     end
     return total_loss
 end
